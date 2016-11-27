b3dbb20912e6e434bc84d8b15e16130516cc5246
Cameron Martin <cameronmartin@davids-air-3.home>
2016-11-27 12:44:27 -0500

    Rebuilt application as master-detail application with split view controller

---
+++ GameOfLife-iOS/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  GameOfLife-iOS
+//
+//  Created by Cameron Martin on 11/27/16.
+//  Copyright © 2016 Jonathan J. Lee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    
+}
---
+++ GameOfLife-iOS/MasterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MasterViewController.swift
+//  GameOfLife-iOS
+//
+//  Created by Cameron Martin on 11/27/16.
+//  Copyright © 2016 Jonathan J. Lee. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+            
+            return cell
+    }
+}
