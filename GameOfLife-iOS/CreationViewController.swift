03157187fd33c1082124174cd5898dc9ed8c6d6e
Jonathan J. Lee <JJL@MacBook-Pro.local>
2016-12-11 20:28:43 -0500

    Added a way to dismiss the keyboard by tapping anywhere else in the CreationViewController.  Fixed a few bugs with keyboard input in which you could still add characters such as $ to the size field.  Fixed a critical error in which the program crashes if you attempt to create a colony without the name or size fields; instead of the button directly calling the unwind segue, it calls the "attempt unwind" method, which will not start the unwind if one of the fields is empty (it also turns the fields red).  Spruced up the creation view with a description of each of the templates.

--- GameOfLife-iOS/CreationViewController.swift
+++ GameOfLife-iOS/CreationViewController.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class CreationViewController: UIViewController, UITextFieldDelegate {
     
+    var colony: Colony?
+    
     @IBOutlet var nameField: UITextField!
     @IBOutlet var sizeField: UITextField!
     @IBOutlet var templateSelector: UISegmentedControl!
     @IBOutlet var saveButton: UIButton!
+    @IBOutlet var templateDescriptionLabel: UILabel!
     
-    var colony: Colony?
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func attemptUnwind() {
+        var doSegue = true
+        if nameField.text! == "" {
+            doSegue = false
+            nameField.backgroundColor = UIColor(red:1.00, green:0.42, blue:0.42, alpha:1.0)
+        }
+        if sizeField.text! == "" {
+            doSegue = false
+            sizeField.backgroundColor = UIColor(red:1.00, green:0.42, blue:0.42, alpha:1.0)
+        }
+        if doSegue {
+            performSegue(withIdentifier: "returnToColonyList", sender: self)
+        }
+    }
+    
+    @IBAction func updateTemplateDescriptionLabel(sender: AnyObject) {
+        let templateNumber = templateSelector.selectedSegmentIndex
+        switch templateNumber {
+        case 1:
+            templateDescriptionLabel.text = "Basic: evolves into a quartet of oscillating bars."
+        case 2:
+            templateDescriptionLabel.text = "Glider Gun: continually shoots out gliders. Minimum size is 40; will automatically increase size to 40 if you set a lesser value."
+        case 3:
+            templateDescriptionLabel.text = "Random: every cell is randomly set to alive or dead."
+        default:
+            templateDescriptionLabel.text = "Blank: a completely empty colony."
+        }
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -24,9 +58,12 @@ class CreationViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        if ((string.rangeOfCharacter(from: NSCharacterSet.punctuationCharacters) != nil) || string.rangeOfCharacter(from: NSCharacterSet.letters) != nil) {
+        if textField == sizeField && string.rangeOfCharacter(from: CharacterSet(charactersIn: "0123456789")) == nil {
             return false
         } else {
+            if textField.backgroundColor != UIColor.white {
+                textField.backgroundColor = UIColor.white
+            }
             return true
         }
     }
